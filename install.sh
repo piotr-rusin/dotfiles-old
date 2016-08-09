e196ac190d38a6cdfc5cfbf97987c940c0d61c2c
Piotr Rusin <piotr.rusin88@gmail.com>
2016-08-09 12:13:12 +0200

    Make the install.sh script use local project directory
    This commit replaces symlinking from a directory into which the remote
    git repository of the project was cloned with creating a symlink to
    the local project directory inside home directory and then symlinking
    files from there.

--- install.sh
+++ install.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
 dotfiles="$HOME/.dotfiles"
+current_dir=$(pwd)
+abort="Aborting installation"
 
-mkdir -p $dotfiles
-
-if [ ! -e $dotfiles/.git ]
-    then
-        echo "Installing dotfiles into $dotfiles"
-        git clone https://github.com/piotr-rusin/dotfiles $dotfiles
+if [ -d $dotfiles ]; then
+    if [ -L $dotfiles ]; then
+        while true; do
+            echo "A link $dotfiles already exists"
+            read -p "Do you want to override it?" yn
+            case $yn in
+                [Yy]* ) break;;
+                [Nn]* ) echo $abort; exit;;
+                * ) echo "Please answer yes or no.";;
+            esac
+        done
     else
-        echo "Updating contents of $dotfiles"
-        cd $dotfiles && git pull
+        echo "$dotfiles already exists, and it's a directory. $abort"
+        exit
+    fi
 fi
+
+echo "Creating a symbolic link from $current_dir to $dotfiles"
+ln -Tsf $(pwd) $dotfiles
 
 lns() {
     destination="$HOME/$2"
