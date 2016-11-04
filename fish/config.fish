5caeed40f302dbd45714a3b5f7e05366cc56b1d4
Piotr Rusin <piotr.rusin88@gmail.com>
2016-11-04 16:08:43 +0100

    Replace autoloaded add_idle_py function
    This commit replaces add_idle_py function implemented in
    fish/functions/add_idle_py.fish with one implemented in config.fish
    file.
    
    The function is an event handler, so to be available for handling
    events it must be defined inside config.fish - fish shell doesn't
    autoload event handlers.
    
    In addition to being defined elsewhere, the new function differs
    slightly from the previous one: it contains a description and performs
    copy operation only when a virtualenv is active.

--- fish/config.fish
+++ fish/config.fish
@@ -5,4 +5,16 @@ function is_no_virtual_env_active
 end
 
 
+function add_idle_py \
+    -d "Add idle.py to a newly created virtual environment" \
+    --on-event virtualenv_did_create
+
+    set -l ERROR_COLOR (set_color --bold red)
+    if is_no_virtual_env_active
+        echo "$ERROR_COLOR No virtualenv is active!"
+    else
+        cp "$HOME/.virtualenvs/idle.py" "$VIRTUAL_ENV/bin"
+    end
+end
+
 eval (python -m virtualfish compat_aliases projects auto_activation)
--- fish/functions/add_idle_py.fish
+++
@@ -1,6 +0,0 @@
-function add_idle_py --on-event virtualenv_did_create
-    set -l ERROR_COLOR (set_color --bold red)
-    if not set -q VIRTUAL_ENV
-        echo "$ERROR_COLOR No virtualenv is active!"
-    cp "$HOME/.virtualenvs/idle.py" "$VIRTUAL_ENV/bin"
-end
