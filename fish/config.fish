b3442493de078c058793a6c14be4404dcfe1fccb
Piotr Rusin <piotr.rusin88@gmail.com>
2016-11-07 09:24:57 +0100

    Refactor virtualenv activation check
    The test is being refactored by replacing call to
    is_no_virtualenv_active function with its body.

--- fish/config.fish
+++ fish/config.fish
@@ -8,7 +8,7 @@ function add_idle_py \
     --on-event virtualenv_did_create
 
     set -l ERROR_COLOR (set_color --bold red)
-    if is_no_virtualenv_active
+    if not set -q VIRTUAL_ENV
         echo "$ERROR_COLOR No virtualenv is active!"
     else
         cp "$HOME/.virtualenvs/idle.py" "$VIRTUAL_ENV/bin"
