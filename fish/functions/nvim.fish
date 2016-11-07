15b9120fde84d917994ad9a3249f419e172c8207
Piotr Rusin <piotr.rusin88@gmail.com>
2016-11-07 19:58:43 +0100

    Improve code formatting

--- fish/functions/nvim.fish
+++ fish/functions/nvim.fish
@@ -1,13 +1,10 @@
-function nvim \
-    -d "Run neovim with support for deoplete and pylama maker"
+function nvim -d "Run neovim with support for deoplete and pylama maker"
 
     begin
-        # python packages required by deoplete are installed
-        # in the virtualenv
+        # python packages required by deoplete are installed in the virtualenv
         set -lx PYTHONPATH $PYTHONPATH \
         "$HOME/.virtualenvs/neovim/lib/python3.5/site-packages"
-        # neomake needs to be able to access pylama installed
-        # in the virtualenv
+        # neomake needs to be able to access pylama installed in the virtualenv
         set -lx PATH $PATH "$HOME/.virtualenvs/neovim/bin"
 
         command nvim
