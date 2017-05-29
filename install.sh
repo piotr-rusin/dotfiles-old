245baddca8ed6f06f7e73a7c7e62953981206d85
Piotr Rusin <piotr.rusin88@gmail.com>
2017-05-29 13:47:24 +0200

    Add configuration for zsh

--- install.sh
+++ install.sh
@@ -24,6 +24,13 @@ mkdir -p ~/.config/composer
 ln -sf $CURRENTDIR/composer.json ~/.config/composer
 ln -sf $CURRENTDIR/.Xresources ~
 
-echo "Configuring git"
+echo "Installing Oh-My-Zsh"
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+rm ~/.zshrc # remove zshrc created by Oh-My-Zsh
 
+echo "Creating a symbolic link to .zshrc"
+ln -sf $CURRENTDIR/.zshrc ~
+
+
+echo "Configuring git"
 git config --global core.editor "nvim -u $HOME/.config/nvim/git_init.vim"
